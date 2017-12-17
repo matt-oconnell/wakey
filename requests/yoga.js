95b02928394f79875d9c0cf360d6f9396a7631a8
matt-oconnell <mattoconnell408@gmail.com>
2017-12-17 10:13:19 -0500

    allow all times for sat/sun yoga

--- requests/yoga.js
+++ requests/yoga.js
@@ -2,16 +2,20 @@ const axios = require('axios')
 const moment = require('moment')
 moment.tz.setDefault('America/New_York')
 
+function filterYogaClasses(classEvent) {
+  return /Yoga/i.test(classEvent.className)
+  && 
+  ((moment().weekday() === 0 || moment().weekday === 6)
+  || (moment(classEvent.timestamp).isSameOrAfter(moment().startOf('day').add(12, 'hours'))))
+}
+
 module.exports = {
   yoga: async () => {
     const today = moment().format('YYYY-MM-DD')
     const tomorrow = moment().add(1, 'days').format('YYYY-MM-DD')
     const url = `https://nyhrc-schedules.64labs.com/calendars/events?clubNumbers=5102&startDate=${today}&endDate=${tomorrow}`
     const yogaResponse = await axios.get(url)
-    return yogaResponse.data.filter(classEvent => {
-      return /Yoga/i.test(classEvent.className)
-        && moment(classEvent.timestamp).isSameOrAfter(moment().startOf('day').add(12, 'hours'));
-    }).map(classEvent => ({
+    return yogaResponse.data.filter(filterYogaClasses).map(classEvent => ({
       class: classEvent.className,
       time: classEvent.time,
       instructor: classEvent.instructorName
